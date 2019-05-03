910592edd1a3d22be0984a96d5d00ea061bfaff4
Jose Medina Rodriguez <jmr779@inlumine.ual.es>
2019-05-03 13:49:30 +0200

    Subida tutorial angular en desarrollo

---
+++ users-app/src/app/mock-users.ts
@@ -0,0 +1,14 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+  { id: 1, name: 'Jose Medina', pass: '123', email: 'jmr779@inlumine.ual.es' },
+  { id: 12, name: 'Alberto', pass: 'abc', email: 'aff125@inlumine.ual.es' },
+  { id: 13, name: 'Alfonso', pass: '123', email: 'acg151@inlumine.ual.es' },
+  { id: 14, name: 'Pedro Picapiedra', pass: 'abc', email: 'jge534@inlumine.ual.es' },
+  { id: 15, name: 'Antoine', pass: '123', email: 'fje498@inlumine.ual.es' },
+  { id: 16, name: 'Peter', pass: 'abc', email: 'fue343@inlumine.ual.es' },
+  { id: 17, name: 'Johan', pass: '123', email: 'fir434@inlumine.ual.es' },
+  { id: 18, name: 'Cedry', pass: 'abc', email: 'rrr433@inlumine.ual.es' },
+  { id: 19, name: 'Wctos', pass: '123', email: 'ref333@inlumine.ual.es' },
+  { id: 20, name: 'Tornado', pass: 'abc', email: 'rew345@inlumine.ual.es' }
+];
