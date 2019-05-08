4ef6d5101bfad0ca34d98ffe5832e301e1e27a6c
Jose Medina Rodriguez <jmr779@inlumine.ual.es>
2019-05-08 12:24:03 +0200

    Agregados arreglos

--- users-app/src/app/dashboard/dashboard.component.ts
+++ users-app/src/app/dashboard/dashboard.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../user.service';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  
   users: User[] = [];
   
   constructor(private userService: UserService) { }
@@ -18,8 +18,7 @@ export class DashboardComponent implements OnInit {
   }
  
   getUsers(): void {
-    this.userService.getUsers()
-      .subscribe(users => this.users = users.slice(1, 5));
+    this.userService.getUsers().subscribe(users => this.users = users.slice(1, 5));
   }
 
 }
